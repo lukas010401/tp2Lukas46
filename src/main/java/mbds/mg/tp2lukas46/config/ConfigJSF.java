0b52cfe0443c287cb4f7fee8c831021a29bddf10
lukas010401 <lukas.randrianarisoa1@gmail.com>
2023-03-27 16:59:10 +0300

    Etat initial

---
+++ src/main/java/mbds/mg/tp2lukas46/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mbds.mg.tp2lukas46.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author lukas
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
+
